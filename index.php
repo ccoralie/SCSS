aa4a104cb9f8bdae9ca7708786a808ddcb4646db
ccoralie <36480192+ccoralie@users.noreply.github.com>
2018-05-22 15:58:33 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8"/>
+    <title>FindThePrecious.com</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+<header>
+
+
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#"><img src="logo.png"/> </a>
+            </div>
+            <ul>
+                <li><a href="#">Fellows</a></li>
+                <li><a href="#">The Ring</a></li>
+                <li><a href="#">Get my reward</a></li>
+                <li><a href="#">Best hunters</a></li>
+                <li><a href="#">Join the army</a></li>
+                <li><a href="#">Contact us</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
+
+<section class="Precious">
+    <div class="item">
+        <div>
+            <div class="ring1"></div>
+            <h3 class="text">  My precious  </h3>
+            <div class="ring1"></div>
+        </div>
+    </div>
+
+
+    <div class="item">
+        <div>
+            <div class="ring2"></div>
+            <h3 class="text">The one ring</h3>
+        </div>
+    </div>
+
+    <div class="item">
+        <div class="text">
+            <div class="ring3_1"></div>
+            <div class="ring3_2"></div>
+            <div class="ring3_3"></div>
+            <div class="ring3_4"></div>
+            <div class="ring3_5"></div>
+            <div class="ring3_6"></div>
+            <h3>Please find it!</h3>
+        </div>
+    </div>
+
+</section>
+</body>
+</html>
